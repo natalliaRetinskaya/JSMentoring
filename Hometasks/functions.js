7961f255fbfa04db211e529717ab7cf51af2b68e
Natallia Ratsinskaya <natallia_ratsinskaya@epam.com>
2020-03-31 15:26:22 +0300

    task 3 w/o last task

---
+++ Hometasks/functions.js
@@ -0,0 +1,97 @@
+/**
+ * write function that adds two numbers
+ *
+ */
+function add(a, b) {
+    return a + b
+}
+
+/**
+ * write function that return first and last name of given object
+ * properties firstName and lastName
+ * {
+ *    firstName: "John"
+ *    lastName: "Dou"
+ * }
+ */
+function getFullName(object) {
+    return (object.firstName + " " + object.lastName);
+}
+
+/**
+ * write fuction that checks is number is odd
+ * true if odd, false if even
+ */
+function isOdd(n) {
+    if (n % 2 !== 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+/**
+ * write function that return shortest of words in given array
+ * e.g ["one", "two", "three"] should return one
+ */
+function getShortest(wordArray) {
+    let newArr = []
+    for (let i = 0; i < wordArray.length; i++){
+    newArr.push(wordArray[i].length)
+    }
+    newArr.sort()
+    return newArr[0]
+}
+
+/**
+ * write function that returns word google with given numbers of "o" symbols
+ * e.g getGoogle(5) should return "gooooogle"
+ */
+function getGoogle(n) {
+    return `G${(new Array(n)).fill('o').join('')}gle`
+}
+// Или обязательно через letter.repeat?
+
+/**
+ * write function that returns object based on given information (params may be null)
+ * getUser("John", "Dou", 42) should return
+ * {
+ *    firstName: "John"
+ *    lastName: "Dou"
+ *    age: 42
+ * }
+ */
+function getUser(firstName = "unknown", lastName = "unknown", age = "unknown") {
+    return obj = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+    }
+}
+
+/**
+ * write function that calculates total path traveled.
+ * path represended as array of objects with field distance and direction
+ * e.g [{direction: "Kiyv - Minsk", distance: 567}, {direction: "Kiyv - Paris", distance: 2402}]
+ */
+
+function getTotalPath(path) {
+    function getTotalPath(path) {
+        let totalPath
+        for (let i = 0; i < path.length; i++) {
+
+        }
+        //return totalPath
+    }
+    
+    console.log(getTotalPath(obj))
+
+module.exports = {
+    add,
+    getFullName,
+    isOdd,
+    getShortest,
+    getGoogle,
+    getUser,
+    getTotalPath,
+};
