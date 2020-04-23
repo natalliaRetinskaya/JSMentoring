fdcdf7d69dbd237115d5369f7196698d9168e112
Natallia <natalliaretinskaya@gmail.com>
2020-04-23 17:18:38 +0300

    async

--- tasks/async/async.js
+++ tasks/async/async.js
@@ -8,38 +8,72 @@ const {
 /**
  * console.log data got from callbackFunction
  */
+
 function printDataCallback() {
+    function printData(data) {
+        console.log(data)
+    }
+    callbackFunction(printData)
 }
 
 /**
  * console.log data got from promiseFunction
  */
 function printDataPromise() {
+    promiseFunction().then((data) => {
+        console.log(data)
+    })
 }
 
 /**
  * console.log data got from promiseFunction using async/await
  */
 async function printDataAsyncAwait() {
+    promiseFunction().then((data) => {
+        console.log(data)
+    })
 }
+
+
 
 /**
  * throw error data from callbackFunctionError
  */
 function handleErrorCallback() {
+    function printData(error) {
+        throw error
+    }
+    callbackFunctionError(printData)
 }
 
 /**
  * throw error come from promiseFunctionError
  */
 function handlePromiseError() {
+    promiseFunctionError.catch();
 }
+
+
+// console.log(promiseFunctionError())
+// promiseFunctionError().then(()=>{}).catch((error) => {
+//     //console.log(error)
+//     throw error
+
+//handlePromiseError()
 
 /**
  * throw error come from promiseFunctionError using async/await
  */
 async function handleAsyncAwaitError() {
-}
+    try {
+        await promiseFunctionError();
+    } catch (Error) {
+        console.error(Error);
+    };
+};
+
+
+
 
 module.exports = {
     printDataCallback,
